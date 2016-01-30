90813ef4a6ceac7c8dc97465ed48c76773a684ef
Maxime BOMPOIL <mbompoil@e1r6p12.42.fr>
2016-01-30 23:10:12 +0100

    resize

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mbompoil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/30 18:06:59 by mbompoil          #+#    #+#             */
-/*   Updated: 2016/01/30 22:50:18 by mbompoil         ###   ########.fr       */
+/*   Updated: 2016/01/30 23:09:19 by mbompoil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,19 @@ int		main(void)
 {
 	int		col;
 	int		line;
-	int		i;
-	int		j;
-
-	j = 1;
-	i = 0;
+	
 	initscr();
 	noecho();
 	raw();
 	getmaxyx(stdscr, col, line);
 	ft_display(col, line);
-	refresh();
-	getch();
+	while (getch() == 410)
+	{
+		clear();
+		getmaxyx(stdscr, col, line);
+		ft_display(col, line);
+		refresh();
+	}
 	getch();
 	endwin();
 	return (0);
