2648cae968c5a6345711b775b585035fa035439e
Maxime BOMPOIL <mbompoil@e1r6p12.42.fr>
2016-01-31 15:56:15 +0100

    display

--- ft_display.c
+++ ft_display.c
@@ -6,30 +6,51 @@
 /*   By: mbompoil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/30 22:06:34 by mbompoil          #+#    #+#             */
-/*   Updated: 2016/01/31 14:46:14 by mbompoil         ###   ########.fr       */
+/*   Updated: 2016/01/31 15:18:32 by mbompoil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
 
+static void		ft_displayn2(int col, int line, int **tab)
+{
+	if (tab[0][2] != 0)
+		mvprintw(col / 8, line * 5 / 8, "%d", tab[0][2]);
+	if (tab[0][3] != 0)
+		mvprintw(col / 8, line * 7 / 8, "%d", tab[0][3]);
+	if (tab[1][2] != 0)
+		mvprintw(col * 3 / 8, line * 5 / 8, "%d", tab[1][2]);
+	if (tab[1][3] != 0)
+		mvprintw(col * 3 / 8, line * 7 / 8, "%d", tab[1][3]);
+	if (tab[2][1] != 0)
+		mvprintw(col * 5 / 8, line * 3 / 8, "%d", tab[2][1]);
+	if (tab[2][3] != 0)
+		mvprintw(col * 5 / 8, line * 7 / 8, "%d", tab[2][3]);
+	if (tab[3][1] != 0)
+		mvprintw(col * 7 / 8, line * 3 / 8, "%d", tab[3][1]);
+	if (tab[3][2] != 0)
+		mvprintw(col * 7 / 8, line * 5 / 8, "%d", tab[3][2]);
+}
+
 static void		ft_displayn(int col, int line, int **tab)
 {
-	mvprintw(col / 8, line / 8, "%d", tab[0][0]);
-	mvprintw(col * 3 / 8, line * 3 / 8, "%d", tab[1][1]);
-	mvprintw(col * 5 / 8, line * 5 / 8, "%d", tab[2][2]);
-	mvprintw(col * 7 / 8, line * 7 / 8, "%d", tab[3][3]);
-	mvprintw(col * 3 / 8, line / 8, "%d", tab[1][0]);
-	mvprintw(col / 8, line * 3 / 8, "%d", tab[0][1]);
-	mvprintw(col * 5 / 8, line / 8, "%d", tab[2][0]);
-	mvprintw(col * 7 / 8, line / 8, "%d", tab[3][0]);
-	mvprintw(col / 8, line * 5 / 8, "%d", tab[0][2]);
-	mvprintw(col / 8, line * 7 / 8, "%d", tab[0][3]);
-	mvprintw(col * 3 / 8, line * 5 / 8, "%d", tab[1][2]);
-	mvprintw(col * 3 / 8, line * 7 / 8, "%d", tab[1][3]);
-	mvprintw(col * 5 / 8, line * 3 / 8, "%d", tab[2][1]);
-	mvprintw(col * 5 / 8, line * 7 / 8, "%d", tab[2][3]);
-	mvprintw(col * 7 / 8, line * 3 / 8, "%d", tab[3][1]);
-	mvprintw(col * 7 / 8, line * 5 / 8, "%d", tab[3][2]);
+	if (tab[0][0] != 0)
+		mvprintw(col / 8, line / 8, "%d", tab[0][0]);
+	if (tab[1][1] != 0)
+		mvprintw(col * 3 / 8, line * 3 / 8, "%d", tab[1][1]);
+	if (tab[2][2] != 0)
+		mvprintw(col * 5 / 8, line * 5 / 8, "%d", tab[2][2]);
+	if (tab[3][3] != 0)
+		mvprintw(col * 7 / 8, line * 7 / 8, "%d", tab[3][3]);
+	if (tab[1][0] != 0)
+		mvprintw(col * 3 / 8, line / 8, "%d", tab[1][0]);
+	if (tab[0][1] != 0)
+		mvprintw(col / 8, line * 3 / 8, "%d", tab[0][1]);
+	if (tab[2][0] != 0)
+		mvprintw(col * 5 / 8, line / 8, "%d", tab[2][0]);
+	if (tab[3][0] != 0)
+		mvprintw(col * 7 / 8, line / 8, "%d", tab[3][0]);
+	ft_displayn2(col, line, tab);
 }
 
 void			ft_displayg(int col, int line, int **box)
